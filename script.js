755183ca4e1d87b76c3a3361fb53f1e84ee6954e
Forresterjamie01 <Forresterjamie01@yahoo.com>
2021-03-31 02:51:46 -0400

    started creating questions in html and css.

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 //What: Create coding quiz on Javascript that stores high scores
 //How: With Javascript that we have learned this week
 //Why: To become familiarized with these requests for on the job assessments
-class quiz
-    static void main(String[]args){
+
 //Question 1 of the Quiz
-class Question ()
-    }
+
